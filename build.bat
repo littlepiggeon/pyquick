a2ac6da4222d763d46522815dc88c8e9c1764de5
how_to_create <fairzlq@hotmail.com>
2024-11-22 21:46:58 +0800

    1.1 prepareing

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 .\pt\Scripts\activite
-python -m nuitka --onefile --windows-console-mode=disable --enable-plugins=tk-inter,upx --windows-icon-from-ico=old.ico --upx-binary=D:\upx.exe --windows-uac-admin --show-progress --windows-company-name=p.t. --windows-product-name=python_tool --windows-file-version=1.1  --include-data-files=.\python_tool=python_tool.ico --mingw64 --remove-output python_tool.py
+python -m nuitka --onefile --windows-console-mode=disable --enable-plugins=tk-inter,upx --windows-icon-from-ico=pythontool.ico --upx-binary=D:\upx.exe --windows-uac-admin --show-progress --windows-company-name=p.t. --windows-product-name=python_tool --windows-file-version=1.1  --include-data-files=.\pythontool.ico=pythontool.ico --mingw64 --remove-output python_tool.py
